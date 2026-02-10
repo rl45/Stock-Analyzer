5a6d9aadab5c1314ca538e45914f8d4ff775fd90
filmboard-io <info.filmboard@gmail.com>
2026-02-09 20:22:51 -0800

    Add API serverless functions

--- api/chart.js
+++ api/chart.js
@@ -1,5 +1,7 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   
   if (req.method === 'OPTIONS') {
     res.status(200).end();
